19564d629da03573ee788141373dedcb163242c4
root <root@raspberrypi.(none)>
2015-06-10 11:16:11 +0000

    nouveau timer

--- proto3/timer.py
+++ proto3/timer.py
@@ -1,20 +1,20 @@
 import variables as v
 
 def init_timer(time):
-	global v.start_timer, v.end_timer
-	start_time = datetime.datetime.now()
-	end_time = start_time + datetime.timedelta(time)
+	global v.start_timer, v.end_time
+	v.start_time = v.datetime.datetime.now()
+	v.end_time = v.start_time + datetime.timedelta(time)
 	return True
 
 def timer_playable() :
-	global v.end_timer, v.now_timer, 
+	global v.end_time, v.now_time, 
 	v.now_timer = datetime.datetime.now()
-	if v.now_timer.hours == v.end_timer.hours and v.now_timer.minutes == v.end_timer.minutes and v.now_timer.seconds == v.end_timer.seconds :
+	if v.now_time > v.end_time
 		 return False
 	return True
 
 def watchdog_timer() :
 	global v.is_playable
 	v.is_playable = timer_playable()
-		
+	v.threading.Timer(1,watcdhog_timer).start()	
 
